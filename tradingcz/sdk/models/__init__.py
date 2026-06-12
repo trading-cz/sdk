e40faec2c0e2660c9169b930e44df0dcd59e95fe
effcod <130288908+effcod@users.noreply.github.com>
2026-06-12 12:29:15 +0200

    migrate: move smoke tests to testing repo; fix broken OrderRequest import

--- tradingcz/sdk/models/__init__.py
+++ tradingcz/sdk/models/__init__.py
@@ -33,7 +33,8 @@ from tradingcz.sdk.models.market import (
     market_item_message_type,
 )
 from tradingcz.sdk.models.events.execution_request_event import ExecutionRequestEvent
-from tradingcz.sdk.models.orders.order import OrderRequest
+# TODO: OrderRequest base class not yet created on this branch
+# from tradingcz.sdk.models.orders.order import OrderRequest
 from tradingcz.sdk.models.orders.oto_order import OtoOrderRequest
 from tradingcz.sdk.models.orders.bracket_order import BracketOrderRequest
 from tradingcz.sdk.models.orders.market_order import MarketOrderRequest
@@ -74,7 +75,7 @@ __all__ = [
     "ServiceRequest",
     # Orders & Signals
     "ExecutionRequestEvent",
-    "OrderRequest",
+    # "OrderRequest",  # TODO: base class not yet created
     "OtoOrderRequest",
     "BracketOrderRequest",
     "MarketOrderRequest",
